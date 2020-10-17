9ad7a32433976d086b1c11bc8a0c96f9b8b98ec1
Emily Le Deunf <emilyledeunf@air-de-emily.home>
2020-10-17 13:54:10 +0200

    Adding material-ui package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import './App.css';
+import {FormControl, MenuItem, Select} from '@material-ui/core';
 
 function App() {
   return (
     <div className="app">
-      <h1>Covid-19 Special France.</h1>
+      <h1>Covid-19 France Tracker.</h1>
+
+      <FormControl className="app__dropdown">
+        <Select
+        variant= "outlined"
+        value="localisation">
+          <MenuItem value="worldwise">Worldwise</MenuItem>
+          <MenuItem value="worldwise">Worldwise</MenuItem>
+          <MenuItem value="worldwise">Worldwise</MenuItem>
+          <MenuItem value="worldwise">Worldwise</MenuItem>
+        </Select>
+      </FormControl>
+
+
+      {/* Header */}
+      {/* Title + select input dropdown field + select input dropdown field */}
+
+      {/* Infobox */}
+      {/* Infobox */}
+      {/* Infobox */}
+
+      {/* Map */}
+
+      {/* Table */}
+      {/* Graph */}
+
     </div>
   );
 }
